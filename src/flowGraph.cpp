35a05ffc7a2dbc06bde795fc8792a1876bd4f138
ranaxarun <141982286+ranaxarun@users.noreply.github.com>
2026-04-25 10:08:54 -0400

    Initialize flowGraph constructor with parameters

--- src/flowGraph.cpp
+++ src/flowGraph.cpp
@@ -1,1 +1,12 @@
 
+#include "flowGraph.h"
+
+
+flowGraph::flowGraph(tbb::flow::graph g ): 
+                                        g(g), 
+                                        src(g,[]()->std::map<int,std::string>{ return map },false),
+                                        fn(g, )
+
+{
+  
+}
