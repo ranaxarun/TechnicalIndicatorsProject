ee7e7f2544a003aeda286689f80faa69e98fc947
ranaxarun <141982286+ranaxarun@users.noreply.github.com>
2026-04-25 09:41:11 -0400

    Add flowGraph class with TBB flow graph components

--- include/flowGraph.h
+++ include/flowGraph.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <tbb/flow_graph.h>
 
+using namespace tbb::flow;
+
+class flowGraph {
+public:
+    tbb::flow::source_node<int> src
+    tbb::flow::graph g;
+    tbb::flow::buffer_node<int>        buf;
+    tbb::flow::function_node<int, int> fn;
+  
+
+    Processor();
+    void run(int input);
+    void regfunction(function_node<int, int>& node, int val);
+};
