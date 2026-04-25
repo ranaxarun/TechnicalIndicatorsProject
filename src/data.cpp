4e80d78fd4256a3652ce506247c6d835b0a0fe4b
ranaxarun <141982286+ranaxarun@users.noreply.github.com>
2026-04-25 14:42:21 -0400

    Refactor tickerData and calculation classes
    Refactor tickerData and calculation constructors, and add placeholder logic for RSI and EMA calculations.

--- src/data.cpp
+++ src/data.cpp
@@ -1,20 +1,26 @@
-#include<data.h>
+#include "data.h"
 #include<iostream>
 #include<string>
 
-tickerData::tickerData(){}
+//tickerData::tickerData(){}
 
-calculation::calculation(){}
+tickerData:: tickerData(std::string id,std::string period,double open,double close,double low, double high):
+    id(id),period(period),open(open),close(close),low(low),high(high){}
 
-calculation::calculation(tickerData stockInfo&):ticker(stockInfo){
+//calculation::calculation(){}
+
+calculation::calculation(tickerData &stockInfo):ticker(stockInfo)
+{
 }
 
-double calculation::calRSIDaily(tickerData stockInfo&)
+double calculation::calRSIDaily(tickerData &stockInfo)
 {
-
+ std::cout<<"RSI logic to be added";
+ return 0.0 ;// this will change after the logic
 }
 
-double calculation::calEMADaily(tickerData stockInfo&)
+double calculation::calEMADaily(tickerData &stockInfo)
 {
-
+   std::cout<<"EMA logic to be added";
+    return 0.0 ;// this will change after the logic
 }
