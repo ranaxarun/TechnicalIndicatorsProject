fa08cfcbbf8afb1be25e5dee7195bb27c886b27e
ranaxarun <141982286+ranaxarun@users.noreply.github.com>
2026-04-25 14:43:11 -0400

    Add main function to initialize tickerData and calculation

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "data.h"
 
+int main()
+{
+    tickerData data ("1","14",12,78,87,65);
+    
+    calculation c (data);
+    
+    std::cout<<"Hello World";
+
+    return 0;
+}
